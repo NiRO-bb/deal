5e28a02803ef01b3abeeddc6ddad2e67078cdfe7
Nikita Borisov <nikita.borisov44444@gmail.com>
2025-07-13 12:27:52 +0400

    chore: remove unused classes

--- src/main/java/com/example/Deal/Repository/CurrencyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Deal.Repository;
-
-import com.example.Deal.DTO.Currency;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CurrencyRepository extends CrudRepository<Currency, String> {
-}
